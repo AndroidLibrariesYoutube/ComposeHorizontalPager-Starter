3f0857933b5a3f17c864156bd81c082db73a91df
dheerajrijhwani <dheerajrijhwani@gmail.com>
2021-10-10 14:52:34 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/composeonboardingstarter/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.composeonboardingstarter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.FloatRange
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import com.example.composeonboardingstarter.ui.theme.AppTheme
+import com.example.composeonboardingstarter.ui.theme.Grey300
+import com.example.composeonboardingstarter.ui.theme.Grey900
+import com.example.composeonboardingstarter.ui.theme.Typography
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
+
+class MainActivity : ComponentActivity() {
+
+    @ExperimentalPagerApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            AppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface() {
+                }
+
+            }
+        }
+    }
+}
+
+
+
---
+++ app/src/main/java/com/example/composeonboardingstarter/OnBoardingData.kt
@@ -0,0 +1,6 @@
+package com.example.composeonboardingstarter
+
+data class OnBoardingData(
+    val image: Int, val title: String,
+    val desc: String
+)
